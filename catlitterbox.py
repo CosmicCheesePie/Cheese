b67b94241347f43a03e8964dff9d9b29a71791fd
CosmicCheesePie <janek.jakub9@proton.me>
2024-08-15 12:29:00 +0200

    Update catlitterbox.py

--- catlitterbox.py
+++ catlitterbox.py
@@ -1,3 +1,4 @@
+#A helpful guideline for cat owners is to have one more litter box than the number of cats you have, So I made a simple calculator (For learning purposes).
 numoflitter = int(input("How many cats do you have? "))
 totalnum = numoflitter + 1 
 print(f"You should have {totalnum} cat box litters.") 
